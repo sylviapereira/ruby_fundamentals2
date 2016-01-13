ebcf464ebc571b7aabe09955fc5e0191e9c169be
Your Name <sylviapereira@gmail.com>
2016-01-13 16:23:26 -0500

    Finish exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,4 @@
-@grocery_list = ["cereal", "tea", "avocados", "Jello", "udon noodles"]
+@grocery_list = ["cereal", "tea", "avocados", "jello", "udon noodles"]
 
 def add_item(n)
   @grocery_list.push(n).to_s
@@ -9,3 +9,28 @@ def add_item(n)
 end
 
 add_item("rice")
+
+puts "----------------"
+
+puts "#{@grocery_list.length} things to buy!"
+
+puts "-----Also-------"
+
+if @grocery_list.include?("bananas")
+  puts "- You need to pick up bananas today."
+else
+  puts "- You DON'T need to pick up bananas today."
+end
+
+puts "- Do NOT forget #{@grocery_list[1]}!"
+
+puts "If you're shopping at that hipster alphabetical store:"
+@grocery_list.sort.each do |item|
+   puts "* " + item
+ end
+
+puts "But of course they won't have jello:"
+@grocery_list.delete("jello")
+@grocery_list.sort.each do |item|
+   puts "* " + item
+ end
